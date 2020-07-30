cab21664fe5c8653e26d55ed7922982e83c4c0c9
Felipe <felipeas314@gmail.com>
2020-07-30 19:24:52 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Primeiro from './components/basicos/Primeiro';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Primeiro></Primeiro>,
   document.getElementById('root')
 );
 
